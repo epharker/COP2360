dc168324a030fc5ef390fdf35f1373bdaf61c604
Emily Harker <156346305+epharker@users.noreply.github.com>
2024-07-14 19:22:12 -0400

    Added Beverages.cs
    For Module 3 Discussion.

---
+++ Beverages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Beverages
+{
+    public class Alcohol
+    {
+        public string Name { get; set; }
+        public string PlaceOfOrigin { get; set; }
+    }
+    public class Wine : Alcohol // Class taken from Classes.txt
+    {
+        public decimal Price;
+        public int Year;
+        public Wine(decimal price) => Price = price;
+        public Wine(decimal price, int year) : this(price) => Year = year;
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Wine exampleWine = new Wine(120.00m, 2021);
+            exampleWine.Name = "Petrolo Boggina";
+            exampleWine.PlaceOfOrigin = "Italy";
+
+            Console.WriteLine("Name: {0}\nPrice: {1:c}\nYear: {2}\nPlace of Origin: {3}", exampleWine.Name, exampleWine.Price, exampleWine.Year, exampleWine.PlaceOfOrigin);
+        }
+    }
+}
