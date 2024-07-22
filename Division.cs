820c7d5f63e7ad40ee6fe596f7bc4521a1e1ed0a
Emily Harker <156346305+epharker@users.noreply.github.com>
2024-07-21 21:13:39 -0400

    Added Division.cs
    For Module 04 Individual Project.

---
+++ Division.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {   
+        Start:
+        try {
+           
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{num1} / {num2} = {Divide(num1, num2)}");
+        }  
+         catch (FormatException ex) { 
+            Console.WriteLine($"Error: FormatException. Method: {ex.TargetSite} -- {ex.Message}\nPlease only enter integers. Try again.");
+            goto Start;
+        }
+        catch (DivideByZeroException ex) { 
+            Console.WriteLine($"Error: DivideByZeroException. Method: {ex.TargetSite} -- {ex.Message}\nCannot divide by zero. Try again.");
+            goto Start;
+        }
+        catch (OverflowException ex) { 
+            Console.WriteLine($"Error: OverflowException. Method: {ex.TargetSite} -- {ex.Message}\nNumber is too large or too small. Try again.");
+            goto Start;
+        }
+        catch (Exception ex) { 
+            Console.WriteLine($"Error: {ex.ToString()}");
+        } 
+        
+        Console.WriteLine("---Press any key to exit.---");
+        Console.ReadKey();
+        
+    }
+
+    static int Divide(int x, int y) {
+        return x / y;
+    }
+}
